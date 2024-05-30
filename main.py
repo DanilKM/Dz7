34a33611634e2852c792d28c57c477b85c51bf06
DanilKM <flakon5568@gmail.com>
2024-05-30 15:32:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-s = -1
-while s < len(my_list)-1:
-    s += 1
-    if  my_list[s] > 0:
-        print(my_list[s])
-    elif my_list[s] == 0:
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i == 1:
         continue
+    is_primes = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_primes = False
+            break
+    if is_primes:
+        primes.append(i)
     else:
-        break
+        not_primes.append(i)
+print("Primes", primes)
+print("Not Primes", not_primes)
